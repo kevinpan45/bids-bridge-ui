2473dc3b3456096056848619e36f0e253e8220b5
KP45 <kevinpan45@163.com>
2024-12-30 15:05:17 +0800

    revert to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'app.html' // may differ from host to host
+		}),
 		alias: {
 			$component: "src/component",
 		},
