c510dfa09060063be281d56241e6915c4888198d
KP45 <kevinpan45@163.com>
2025-01-02 11:34:44 +0800

    refactor: update tailwind.config.js to CommonJS module syntax and add .env*.local to .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-export const content = ['./src/**/*.{svelte,js,ts}'];
-export const plugins = [require('daisyui')];
-export const daisyui = {
-    // first theme in this list is the default theme
-    themes: ["dark"],
+module.exports = {
+    content: ['./src/**/*.{svelte,js,ts}'],
+    plugins: [require('daisyui')],
+    daisyui: {
+        // first theme in this list is the default theme
+        themes: ["dark"],
+    },
 };
