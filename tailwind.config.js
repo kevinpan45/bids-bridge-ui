ef5686ae8a6b512b7886accaf5e65a491c5e8725
KP45 <kevinpan45@163.com>
2025-01-02 11:24:21 +0800

    refactor: convert tailwind.config.js to ES module syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-    content: ['./src/**/*.{svelte,js,ts}'],
-    plugins: [require('daisyui')],
-    daisyui: {
-        // first theme in this list is the default theme
-        themes: ["dark"],
-    },
+export const content = ['./src/**/*.{svelte,js,ts}'];
+export const plugins = [require('daisyui')];
+export const daisyui = {
+    // first theme in this list is the default theme
+    themes: ["dark"],
 };
