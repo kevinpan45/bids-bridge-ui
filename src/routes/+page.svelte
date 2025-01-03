e80fea8189e31565b6f0d89bbe8f9cad385a9f6e
KP45 <kevinpan45@163.com>
2025-01-03 19:23:25 +0800

    add marketplace data source and add dataset/pipeline page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
 <script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+    onload = () => {
+        console.log(321);
+        
+    };
+
+    onMount(() => {
+        // axios.get('https://dev-nhrujw8rbt87hkeb.us.auth0.com/.well-known/jwks.json').then(res => {
+        //     console.log(res.data);
+        // });
+        // axios.post('https://dev-nhrujw8rbt87hkeb.us.auth0.com/authorize').then(res => {
+        //     console.log(res.status);
+            
+        // });
+    });
 </script>
 
 <div>BIDS Marketplace</div>
