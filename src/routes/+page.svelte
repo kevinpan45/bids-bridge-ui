75a18817bc98728f816630daa78910806f063aae
KP45 <kevinpan45@163.com>
2025-01-03 19:29:11 +0800

    remove useless test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,9 @@
   import { onMount } from "svelte";
 
     onload = () => {
-        console.log(321);
-        
     };
 
     onMount(() => {
-        // axios.get('https://dev-nhrujw8rbt87hkeb.us.auth0.com/.well-known/jwks.json').then(res => {
-        //     console.log(res.data);
-        // });
-        // axios.post('https://dev-nhrujw8rbt87hkeb.us.auth0.com/authorize').then(res => {
-        //     console.log(res.status);
-            
-        // });
     });
 </script>
 
