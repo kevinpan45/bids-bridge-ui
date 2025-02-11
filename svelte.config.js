4373f67821428127cc7f234c314a948c4126f825
KP45 <kevinpan45@163.com>
2025-02-11 16:46:10 +0800

    add support for static site generation with SvelteKit adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-vercel';
+import adapterStatic from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x',
-		}),
+		adapter: process.env.SVELTEKIT_BUILD_TYPE === 'static'
+			? adapterStatic({
+				fallback: 'app.html' // may differ from host to host
+			})
+			: adapter({
+				runtime: 'nodejs20.x',
+			}),
 		alias: {
 			$component: "src/component",
 			$lib: "src/lib",
