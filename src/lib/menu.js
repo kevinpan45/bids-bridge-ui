75159b7e970e0badbe80b003c6a9c94b3f3a276e
KP45 <kevinpan45@163.com>
2025-03-08 20:41:55 +0800

    add job page

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -1,5 +1,6 @@
 let menus = [
     { name: "marketplace", link: "/marketplace", alias: "Marketplace", icon: "marketplace", enabled: true },
+    { name: "job", link: "/job", alias: "Job", icon: "job", enabled: true },
     { name: "pipeline", link: "/pipeline", alias: "Pipeline", icon: "pipeline", enabled: true },
     { name: "dataset", link: "/dataset", alias: "Dataset", icon: "dataset", enabled: true },
     { name: "storage", link: "/storage", alias: "Storage", icon: "storage", enabled: true },
--- src/routes/job/+page.svelte
+++ src/routes/job/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   import axios from "axios";
   import { onMount } from "svelte";
+  import TimeView from "$component/TimeView.svelte";
 
   let jobs = [];
 
   onMount(() => {
-    axios.get("/api/jobs").then((response) => {
+    axios.get("/api/bff/jobs").then((response) => {
       jobs = response.data;
     });
   });
@@ -30,13 +31,23 @@
       <tr>
         <td>{job.id}</td>
         <td>{job.name}</td>
-        <td>{job.group ?? '-'}</td>
-        <td>{job.status}</td>
-        <td>{job.pipelineId}</td>
-        <td>{job.datasetId}</td>
+        <td>{job.group ?? "-"}</td>
+        <td>
+          {#if job.status === "FINISHED"}
+            <div class="badge badge-success">{job.status}</div>
+          {:else if job.status === "MANUAL_STOPPED" || job.status === "FAILED" || job.status === "ABNORMAL_STOPPED"}
+            <div class="badge badge-error">{job.status}</div>
+          {:else if job.status === "DELETED"}
+            <div class="badge badge-neutral">{job.status}</div>
+          {:else}
+            <div class="badge badge-info">{job.status}</div>
+          {/if}
+        </td>
+        <td>{job.pipelineName}:{job.pipelineVersion}</td>
+        <td>{job.datasetDoi}</td>
         <td>{job.engineJobId}</td>
-        <td>{job.artifactId ?? '-'}</td>
-        <td>{job.createdAt}</td>
+        <td>{job.artifactId ?? "-"}</td>
+        <td><TimeView datetime={job.createdAt} /></td>
       </tr>
     {/each}
   </tbody>
