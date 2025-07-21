a30cfedda5e75cd59c5029e4970c210c0199e2cc
KP45 <kevinpan45@163.com>
2025-07-21 11:17:15 +0800

    update user profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,3 +1,60 @@
-<div>
-  Profile Page
+<script>
+  // get current logged user
+  import { onMount } from "svelte";
+  import toast from "svelte-french-toast";
+  import { user } from "$lib/auth.js";
+
+  onMount(() => {
+    // Check if the user is authenticated
+    if (!user) {
+      toast.error("You must be logged in to access this page.");
+      // Redirect to login page after 1 second
+      setTimeout(() => {
+        window.location.href = "/login";
+      }, 1000);
+    }
+  });
+</script>
+
+<div class="hero">
+  <div class="hero-content w-2/3 flex-col lg:flex-row-reverse">
+    <div class="card bg-base-200 w-full shrink-0 shadow-2xl">
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <form class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            class="input input-bordered"
+            required
+            bind:value={user.name}
+          />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            class="input input-bordered"
+            required
+            bind:value={user.email}
+          />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Nickname</span>
+          </label>
+          <input
+            type="text"
+            class="input input-bordered"
+            required
+            bind:value={user.nickname}
+          />
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
